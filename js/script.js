e532a6f2ed3795e5f0edea7f5635b2092151ee48
OTalDoJesus <72572284+OTalDoJesus@users.noreply.github.com>
2020-11-17 22:41:13 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
 var docHeight = Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
 
+//Adiciona classes nos elementos pais
 function switchMode(){
-    document.getElementById("cabeçalho").classList.toggle("dark");
-    document.getElementById("rodape").classList.toggle("dark");
     document.querySelector("body").classList.toggle("dark");
-    document.getElementById("conteudo").classList.toggle("dark");
+    document.querySelector("modal-bg").classList.toggle("dark");
+    document.querySelector(".menu").classList.toggle("dark");
 }
 
+//Controla o progresso da barra
 function barUpdate(){
+
+    /*Faz umas contas lokas para saber o percentual da página que vc tá e atribui esse percentual 
+    para a width da barra de progesso*/
     var screenPosition = window.innerHeight;
-    var htmlTop = document.querySelector("header").getBoundingClientRect().top;
+    var htmlTop = document.querySelector("body").getBoundingClientRect().top;
     var atual = Math.abs(htmlTop) + screenPosition; 
     var percent = atual * 100 / docHeight;
 
     document.getElementById("progress-bar").style.width = percent + "%";
 }
-barUpdate();
-window.addEventListener("scroll", barUpdate);
+barUpdate(); //Chama a função no load da página
+window.addEventListener("scroll", barUpdate); //Sempre que um scroll é efetuado chama a função
+
+function callMenu(){
+    document.querySelector(".menu").classList.toggle("open");
+}
